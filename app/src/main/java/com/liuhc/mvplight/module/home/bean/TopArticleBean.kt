9fd5a12fae3aae82ce775515e901b85c6336e7fb
liuhc <i_kakaxi@163.com>
2020-10-15 11:21:25 +0800

    增加方法

--- app/src/main/java/com/liuhc/mvplight/module/home/bean/TopArticleBean.kt
+++ app/src/main/java/com/liuhc/mvplight/module/home/bean/TopArticleBean.kt
@@ -1,22 +1,41 @@
 package com.liuhc.mvplight.module.home.bean
 
 data class TopArticleBean(
-    val children: List<Children>?,
+    val apkLink: String?,
+    val audit: Int?,
+    val author: String?,
+    val canEdit: Boolean?,
+    val chapterId: Int?,
+    val chapterName: String?,
+    val collect: Boolean?,
     val courseId: Int?,
+    val desc: String?,
+    val descMd: String?,
+    val envelopePic: String?,
+    val fresh: Boolean?,
     val id: Int?,
-    val name: String?,
-    val order: Int?,
-    val parentChapterId: Int?,
-    val userControlSetTop: Boolean?,
-    val visible: Int?
+    val link: String?,
+    val niceDate: String?,
+    val niceShareDate: String?,
+    val origin: String?,
+    val prefix: String?,
+    val projectLink: String?,
+    val publishTime: Long?,
+    val realSuperChapterId: Int?,
+    val selfVisible: Int?,
+    val shareDate: Long?,
+    val shareUser: String?,
+    val superChapterId: Int?,
+    val superChapterName: String?,
+    val tags: List<Tag?>?,
+    val title: String?,
+    val type: Int?,
+    val userId: Int?,
+    val visible: Int?,
+    val zan: Int?
 ) {
-    data class Children(
-        val courseId: Int?,
-        val id: Int?,
-        val name: String?,
-        val order: Int?,
-        val parentChapterId: Int?,
-        val userControlSetTop: Boolean?,
-        val visible: Int?
+    data class Tag(
+        val name: String,
+        val url: String
     )
 }
--- app/src/main/java/com/liuhc/mvplight/module/home/presenter/HomePresenter.kt
+++ app/src/main/java/com/liuhc/mvplight/module/home/presenter/HomePresenter.kt
@@ -3,8 +3,11 @@ package com.liuhc.mvplight.module.home.presenter
 import com.liuhc.library.presenter.BasePresenter
 import com.liuhc.library.presenter.view.BaseView
 import com.liuhc.mvplight.App
-import com.liuhc.mvplight.module.home.bean.HomeBean
 import com.liuhc.mvplight.common.ServerApi
+import com.liuhc.mvplight.module.home.bean.BannerBean
+import com.liuhc.mvplight.module.home.bean.HomeBean
+import com.liuhc.mvplight.module.home.bean.TopArticleBean
+import com.liuhc.mvplight.module.me.bean.CollectBean
 import kotlinx.coroutines.CoroutineScope
 
 /**
@@ -16,12 +19,82 @@ class HomePresenter(baseView: BaseView, scope: CoroutineScope) :
     BasePresenter(baseView, scope) {
 
     /**
-     * 获取首页文章列表
+     * 置顶文章
+     */
+    fun getTopArticle(callback: (List<TopArticleBean>) -> Unit) {
+        launchUI {
+            val topArticleList = App.retrofit.create(ServerApi::class.java).getTopArticle().check()
+            callback(topArticleList)
+        }
+    }
+
+    /**
+     * 首页文章列表
      */
     fun getHome(page: Int, callback: (HomeBean) -> Unit) {
         launchUI {
             val homeBean = App.retrofit.create(ServerApi::class.java).getHomeArticle(page).check()
             callback(homeBean)
+        }
+    }
+
+    /**
+     * 首页banner
+     */
+    fun getBanner(callback: (List<BannerBean>) -> Unit) {
+        launchUI {
+            val bannerList = App.retrofit.create(ServerApi::class.java).getBanner().check()
+            callback(bannerList)
+        }
+    }
+
+    /**
+     * 收藏文章列表
+     */
+    fun getCollect(page: Int, callback: (CollectBean) -> Unit) {
+        launchUI {
+            val collectBean = App.retrofit.create(ServerApi::class.java).getCollect(page).check()
+            callback(collectBean)
+        }
+    }
+
+    /**
+     * 收藏站内文章
+     */
+    fun addInSiteCollect(id: Int, callback: (Boolean) -> Unit) {
+        launchUI {
+            val baseBean = App.retrofit.create(ServerApi::class.java).addInSiteCollect(id)
+            callback(baseBean.isSuccess())
+        }
+    }
+
+    /**
+     * 收藏站外文章
+     */
+    fun addOutSiteCollect(title: String, author: String, link: String, callback: (Boolean) -> Unit) {
+        launchUI {
+            val baseBean = App.retrofit.create(ServerApi::class.java).addOutSiteCollect(title, author, link)
+            callback(baseBean.isSuccess())
+        }
+    }
+
+    /**
+     * 取消收藏(文章列表)
+     */
+    fun cancelCollect(id: Int, callback: (Boolean) -> Unit) {
+        launchUI {
+            val baseBean = App.retrofit.create(ServerApi::class.java).cancelCollect(id)
+            callback(baseBean.isSuccess())
+        }
+    }
+
+    /**
+     * 取消收藏(我的收藏页面)
+     */
+    fun cancelCollect(id: String, originId: String, callback: (Boolean) -> Unit) {
+        launchUI {
+            val baseBean = App.retrofit.create(ServerApi::class.java).cancelCollect(id, originId)
+            callback(baseBean.isSuccess())
         }
     }
 }
