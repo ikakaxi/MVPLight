56cbf09d75179a874138beb3275befbcf36ada92
liuhc <i_kakaxi@163.com>
2020-10-21 18:19:28 +0800

    GlideApp默认使用RGB_565的图片质量,并且默认禁用硬件位图

--- library/src/main/java/com/liuhc/library/glide/LibraryAppGlideModule.kt
+++ library/src/main/java/com/liuhc/library/glide/LibraryAppGlideModule.kt
@@ -2,12 +2,16 @@ package com.liuhc.library.glide
 
 import android.content.Context
 import com.bumptech.glide.Glide
+import com.bumptech.glide.GlideBuilder
 import com.bumptech.glide.Registry
 import com.bumptech.glide.annotation.GlideModule
 import com.bumptech.glide.integration.okhttp3.OkHttpUrlLoader
+import com.bumptech.glide.load.DecodeFormat
 import com.bumptech.glide.load.model.GlideUrl
 import com.bumptech.glide.module.AppGlideModule
+import com.bumptech.glide.request.RequestOptions
 import java.io.InputStream
+
 
 /**
  * 描述:
@@ -28,5 +32,13 @@ class LibraryAppGlideModule :AppGlideModule() {
 
     override fun registerComponents(context: Context, glide: Glide, registry: Registry) {
         registry.replace(GlideUrl::class.java, InputStream::class.java, OkHttpUrlLoader.Factory())
+    }
+
+    override fun applyOptions(context: Context, builder: GlideBuilder) {
+        builder.setDefaultRequestOptions(
+            RequestOptions()
+                .format(DecodeFormat.PREFER_RGB_565)
+                .disallowHardwareConfig()
+        )
     }
 }
