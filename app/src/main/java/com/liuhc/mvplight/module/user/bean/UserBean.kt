9e26dfc88b6fbd092b5d2c2ecc593e0fb5022bdf
baixiaobing <baixiaobing@haodai.net>
2020-10-26 17:40:38 +0800

    处理注册业务逻辑

--- app/src/main/java/com/liuhc/mvplight/module/user/bean/UserBean.kt
+++ app/src/main/java/com/liuhc/mvplight/module/user/bean/UserBean.kt
@@ -1,23 +1,30 @@
 package com.liuhc.mvplight.module.user.bean
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.util.*
+
 /**
  *
  * @Description:
  * @author: bxb
  * @Date: 2020/10/15
  */
+@Entity(tableName = "users")
 data class UserBean(
+    @PrimaryKey
+    @ColumnInfo(name = "userId") val id: String = UUID.randomUUID().toString(),
+    @ColumnInfo(name = "username") val username: String,
     val admin: Boolean?,
     val chapterTops: List<Any>?,
     val coinCount: Int?,
     val collectIds: List<Int>?,
     val email: String?,
     val icon: String?,
-    val id: Int?,
     val nickname: String?,
     val password: String?,
     val publicName: String?,
     val token: String?,
     val type: Int?,
-    val username: String?
 )
